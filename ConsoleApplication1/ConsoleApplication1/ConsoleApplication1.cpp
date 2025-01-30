c59b41ea0ee8ca52a7fb793bd61700d21af88869
Idkreally-cpu <oaoalvlva@gmail.com>
2025-01-30 10:59:06 +0700

    Commit
    John commit

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Car {
+    string brand;
+    int melieage;
+    string Engine;
+};
+
+int main()
+{
+    Car cars[5] = {
+        {"Toyota", 15000, "автомат"},
+        {"Honda", 30000, "механика"},
+        {"Ford", 25000, "автомат"},
+        {"Toyota", 15000, "механика"},
+        {"BMW", 18000, "автомат"}
+    };
+
+    string InputBrand = "Toyota";
+    int InputMileage = 20000;
+    string inputEngineType = "Automatic";
+
+    for (int i = 0; i < 5; i++) {
+
+    }
+
+}
+
