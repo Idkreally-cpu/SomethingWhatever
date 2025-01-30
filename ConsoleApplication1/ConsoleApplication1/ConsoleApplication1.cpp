f5efeb1e3ae6283743bc55b21eba0b73a5a9254b
Idkreally-cpu <oaoalvlva@gmail.com>
2025-01-30 13:10:37 +0700

    John Commit 2
    Hahha

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,27 +5,31 @@ using namespace std;
 
 struct Car {
     string brand;
-    int melieage;
-    string Engine;
+    int mileage;
+    string engine;
 };
 
 int main()
 {
     Car cars[5] = {
-        {"Toyota", 15000, "автомат"},
-        {"Honda", 30000, "механика"},
-        {"Ford", 25000, "автомат"},
-        {"Toyota", 15000, "механика"},
-        {"BMW", 18000, "автомат"}
+        {"Toyota", 15000, "Automatic"},
+        {"Honda", 30000, "Mechanic"},
+        {"Ford", 25000, "Automatic"},
+        {"Toyota", 15000, "Mechanic"},
+        {"BMW", 18000, "Automatic"}
     };
 
-    string InputBrand = "Toyota";
-    int InputMileage = 20000;
+    string inputBrand = "Toyota";
+    int inputMileage = 20000;
     string inputEngineType = "Automatic";
 
     for (int i = 0; i < 5; i++) {
-
+        if ((cars[i].brand == inputBrand || inputBrand.empty()) &&
+            (cars[i].mileage <= inputMileage || inputMileage == 0) &&
+            (cars[i].engine == inputEngineType || inputEngineType.empty())) {
+            cout << "Brand: " << cars[i].brand << ", Mileage: " << cars[i].mileage
+                << ", Engine: " << cars[i].engine << endl;
+        }
     }
-
 }
 
